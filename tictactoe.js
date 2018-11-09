4802d8822e913120ed288d8f90e8b2743d7fa152
Elgaoussi Hamza <rei-zer0@live.com>
2018-11-09 14:07:34 +0100

    Fix an unknown error in invincible mode

--- tictactoe.js
+++ tictactoe.js
@@ -111,14 +111,16 @@ TicTacToe.prototype.computerPlay = function() {
 
 		// First Move
 		if (that.numPlays === 0)
-			return [0, 2, 4, 6, 8][Math.floor(Math.random() * 5)];
+			return [0, 2, 4, 8][Math.floor(Math.random() * 5)];
 
 		// Second Move
 		if (that.numPlays === 1)
-			if (that.board[4] === "")
-				return 4;
-			else
-				return [0, 2, 6, 8][Math.floor(Math.random() * 4)];
+			if (that.board[4] === "") {
+        return 4;
+      } else {
+        // temporary fix for an unknown error by removing 4
+        return [0, 2, 6, 8][Math.floor(Math.random() * 4)];
+      }
 
 		// Find the empty spot
 		function searchEmpty(i, j, k) {
